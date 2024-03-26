08a20a51f264f1b24a9db17e497ae55dc1b3f7c7
Korjubzot <korjubzot@gmail.com>
2024-03-26 16:04:50 +0100

    ACTUALLY restyled Blog with Tailwind

--- src/components/blog/blog.js
+++ src/components/blog/blog.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Blog() {
   return (
-    <div className="page-container">
-      <div className="blog-container">
+    <div className="flex justify-center items-center text-center m-auto h-screen">
+      <div className="flex flex-col justify-center text-center items-center">
         <h1 className="text-6xl">Pardon our dust</h1>
         <h2 className="text-4xl">My blog is still under construction.</h2>
         <p className="text-2xl">
