ddd2ab75cf4c9e695831ea22870f5a38f3237705
Korjubzot <korjubzot@gmail.com>
2024-03-26 15:25:28 +0100

    Restyled Sidebar component

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,31 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import "./sidebar.css";
 
 import HomeIcon from "@mui/icons-material/Home";
 import EmailIcon from "@mui/icons-material/Email";
 import WorkIcon from "@mui/icons-material/Work";
 import DynamicFeedIcon from "@mui/icons-material/DynamicFeed";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import { LinkedIn } from "@mui/icons-material";
 
+// TODO: Make the sidebar collapsible
 const Sidebar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
-    <div>
-      <button
-        onClick={toggleSidebar}
-        className={`hamburger-icon ${isOpen ? "open" : ""}`}
-      >
-        <MenuOutlinedIcon />
-      </button>
-      <div className={`sidebar ${isOpen ? "open" : ""}`}>
+    <div className="flex flex-col items-center justify-center h-screen w-32 bg-gray-200">
+      <div className="space-y-4">
         <Link to="/" className="sidebar-item">
           <HomeIcon />
           <p> Home</p>
